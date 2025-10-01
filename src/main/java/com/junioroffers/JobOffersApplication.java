973b71ac4412d65a75ab49da4b65e02f38502043
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-10-01 15:07:48 +0200

    our main @SpringBootApplication class

---
+++ src/main/java/com/junioroffers/JobOffersApplication.java
@@ -0,0 +1,13 @@
+package com.junioroffers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+public class JobOffersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobOffersApplication.class, args);
+    }
+}
